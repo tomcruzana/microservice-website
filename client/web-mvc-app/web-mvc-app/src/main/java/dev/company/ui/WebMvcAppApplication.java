dc172878912b70fb0b2ebb72da30c470e2e70da8
tomcruzana <tomcruzana@ymail.com>
2022-05-04 04:08:31 -0700

    updated dirs & added cx cart page

---
+++ client/web-mvc-app/web-mvc-app/src/main/java/dev/company/ui/WebMvcAppApplication.java
@@ -0,0 +1,14 @@
+package dev.company.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcAppApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
