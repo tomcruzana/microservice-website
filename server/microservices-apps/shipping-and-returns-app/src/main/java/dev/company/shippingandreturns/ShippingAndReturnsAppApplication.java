de562c0faebee6bf855b28ddbd0ad6266af3c9ea
tomcruzana <tomcruzana@ymail.com>
2022-05-08 01:03:09 -0700

    added shipping & returns backend

---
+++ server/microservices-apps/shipping-and-returns-app/src/main/java/dev/company/shippingandreturns/ShippingAndReturnsAppApplication.java
@@ -0,0 +1,13 @@
+package dev.company.shippingandreturns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingAndReturnsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingAndReturnsAppApplication.class, args);
+	}
+
+}
---
+++ server/microservices-apps/shipping-and-returns-app/src/test/java/dev/company/shippingandreturns/ShippingAndReturnsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.company.shippingandreturns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingAndReturnsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
