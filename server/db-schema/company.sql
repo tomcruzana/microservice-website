8d6da8bcd8cb2892e304fdee1a177394a403dabf
tomcruzana <tomcruzana@ymail.com>
2022-05-09 10:54:38 -0700

    combined address fields into company table

--- server/db-schema/company.sql
+++ server/db-schema/company.sql
@@ -10,7 +10,12 @@ create table company(
 	slogan tinytext not null,
 	business_hour_start time not null default "08:00:00",
     business_hour_end time not null default "05:00:00",
-    business_days char(20)
+    business_days char(20),
+	street varchar(255) not null,
+	zip char(10) not null,
+    state char(2) not null,
+    country varchar(255) not null,
+    city varchar(255) not null
 );
 
 insert into company
@@ -20,7 +25,8 @@ owner_last_name,
 email_address,
 phone_number,
 slogan,
-business_days
+business_days,
+street, zip, state, country, city
 )
 values
 (
@@ -29,26 +35,8 @@ values
 "3609991234",
 "thomas.developer@email.com",
 "take it or leave it, we still love your business! please come again.",
-"M - F"
+"M - F",
+"3550 southwest ave", "38225", "SE", "United Stated of Japan", "Heineken"
 );
 
 select * from company;
-
-### COMPANY ADDRESS
-create table company_address(
-	id int primary key auto_increment,
-    street varchar(255) not null,
-	zip char(10) not null,
-    state char(2) not null,
-    country varchar(255) not null,
-    city varchar(255) not null,
-    company_id int not null,
-    
-    foreign key (company_id) references company(id)
-);
-
-insert into company_address (street, zip, state, country, city, company_id)
-values ("3550 southwest ave", "38225", "SE", "United Stated of Japan", "Heineken", 1);
-
-select * from company_address;
-select * from company inner join company_address on company.id = company_address.id;
