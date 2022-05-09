10ea64081ea2491d6566965a8fb21b38069474b2
tomcruzana <tomcruzana@ymail.com>
2022-05-09 10:50:25 -0700

    created company info backend

---
+++ server/microservices-apps/company-app/src/main/java/dev/company/entity/Address.java
@@ -0,0 +1,58 @@
+package dev.company.entity;
+
+public class Address {
+
+	private String street;
+
+	private String zip;
+
+	private String state;
+
+	private String country;
+
+	private String city;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public Address() {
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+}
---
+++ server/microservices-apps/company-app/src/main/java/dev/company/entity/Company.java
@@ -0,0 +1,118 @@
+package dev.company.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="company")
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="owner_first_name")
+	private String ownerFirstName;
+
+	@Column(name="owner_last_name")
+	private String ownerLastName;
+
+	@Column(name="email_address")
+	private String emailAddress;
+
+	@Column(name="phone_number")
+	private String phoneNumber;
+
+	private String slogan;
+
+	@Column(name="business_hour_start")
+	private Date businessHourStart;
+
+	@Column(name = "business_hour_end")
+	private Date businessHourEnd;
+
+	@Column(name="business_days")
+	private String businessDays;
+
+	public Company() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getOwnerFirstName() {
+		return ownerFirstName;
+	}
+
+	public void setOwnerFirstName(String ownerFirstName) {
+		this.ownerFirstName = ownerFirstName;
+	}
+
+	public String getOwnerLastName() {
+		return ownerLastName;
+	}
+
+	public void setOwnerLastName(String ownerLastName) {
+		this.ownerLastName = ownerLastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getSlogan() {
+		return slogan;
+	}
+
+	public void setSlogan(String slogan) {
+		this.slogan = slogan;
+	}
+
+	public Date getBusinessHourStart() {
+		return businessHourStart;
+	}
+
+	public void setBusinessHourStart(Date businessHourStart) {
+		this.businessHourStart = businessHourStart;
+	}
+
+	public Date getBusinessHourEnd() {
+		return businessHourEnd;
+	}
+
+	public void setBusinessHourEnd(Date businessHourEnd) {
+		this.businessHourEnd = businessHourEnd;
+	}
+
+	public String getBusinessDays() {
+		return businessDays;
+	}
+
+	public void setBusinessDays(String businessDays) {
+		this.businessDays = businessDays;
+	}
+
+}
