e170211278dc3aebff2f4004a4e0a324ea5e2e97
tomcruzana <tomcruzana@ymail.com>
2022-05-10 12:16:13 -0700

    added entity classes for customer app

--- server/microservices-apps/customer-app/src/main/java/dev/company/customer/entity/Customer.java
+++ server/microservices-apps/customer-app/src/main/java/dev/company/customer/entity/Customer.java
@@ -1,5 +1,54 @@
 package dev.company.customer.entity;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Entity
+@Table(name = "customer")
+public abstract class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	public Customer() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 }
--- server/microservices-apps/customer-app/src/main/java/dev/company/customer/entity/RegisteredCustomer.java
+++ server/microservices-apps/customer-app/src/main/java/dev/company/customer/entity/RegisteredCustomer.java
@@ -1,5 +1,93 @@
 package dev.company.customer.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "registered_customer")
 public class RegisteredCustomer extends Customer {
+
+	@Temporal(TemporalType.DATE)
+	@CreationTimestamp
+	@Column(name = "date_created")
+	private Date dateCreated;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+	@Column(name = "email_address")
+	private String emailAddress;
+
+	private String username;
+
+	private String password;
+
+	@Column(name = "is_enabled")
+	private boolean isEnabled;
+
+	public RegisteredCustomer() {
+		super();
+	}
+
+	@PrePersist
+	public void prePersistDefault() {
+		this.isEnabled = true;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
 
 }
