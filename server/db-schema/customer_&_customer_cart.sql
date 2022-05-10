d951db7d7b13689d742a983e185d11a4fc935c22
tomcruzana <tomcruzana@ymail.com>
2022-05-10 11:25:54 -0700

    modified customer db & added stored procedure

--- server/db-schema/customer_&_customer_cart.sql
+++ server/db-schema/customer_&_customer_cart.sql
@@ -1,19 +1,9 @@
-### CUSTOMER CART
-drop table customer_cart;
+### REGISTERED CUSTOMER ###
+drop table registered_customer;
 
-create table customer_cart(
-	id int primary key
-);
-
-select * from customer_cart;
-
-
-### CUSTOMER 
-drop table customer;
-
-create table customer(
+create table registered_customer(
 	id int primary key auto_increment,
-    customer_cart_id int unique,
+    cart_id char(8) unique,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
     phone_number char(32) unique,
@@ -21,11 +11,10 @@ create table customer(
 	date_created date default (current_date),
     username varchar(255) not null unique,
     password varchar(255) not null,
-    is_enabled boolean not null default true,
-    foreign key (customer_cart_id) references customer_cart(id)
+    is_enabled boolean not null default true
 );
 
-insert into customer
+insert into registered_customer
 (
 first_name,
 last_name,
@@ -44,8 +33,46 @@ values
 "$2a$10$rLH7IADK.DM/QjvGYCt2T.8uwxoZwf1ju4OG54B2hhtPsUr/QnLnm"
 );
 
-### cart creation logic
-insert into customer_cart(id) values((select id from customer where username = "tomdev09"));
-update customer set customer_cart_id = 1 where id = 1;
+select * from registered_customer;
 
-select * from customer;
+
+### CUSTOMER CART ###
+drop table cart;
+
+create table cart(
+	id char(8) primary key,
+    registered_customer_id int not null,
+    
+    foreign key (registered_customer_id) references registered_customer(id)
+);
+
+select * from cart;
+
+### add fk constraint
+alter table registered_customer
+add foreign key (cart_id) references cart(id);
+
+
+### CUSTOMER (NOTE: THIS REPRESENTS AN ABSTRACT CLASS) ###
+drop table customer;
+
+create table customer(
+	id int primary key auto_increment,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null
+);
+
+
+### STORED PROCEDURE : CART CREATION FOR REGISTERED CX ###
+DELIMITER //
+CREATE PROCEDURE CreateCartForRegisteredCustomer(
+	IN cartId char(8),
+    IN registeredCustomerId int
+)
+BEGIN
+	insert into cart(id, registered_customer_id) values(cartId, registeredCustomerId);
+	update registered_customer set cart_id = cartId where id = cartId;
+END //
+DELIMITER ;
+
+CALL CreateCartForRegisteredCustomer('1', 1);
