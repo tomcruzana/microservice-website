300f61af2e3067e48228414807082dc5fb2a4ffa
tomcruzana <tomcruzana@ymail.com>
2022-05-11 15:28:21 -0700

    create a native query

--- server/microservices-apps/customer-app/src/main/java/dev/company/customer/repository/CartItemRepository.java
+++ server/microservices-apps/customer-app/src/main/java/dev/company/customer/repository/CartItemRepository.java
@@ -1,9 +1,15 @@
 package dev.company.customer.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import dev.company.customer.entity.CartItem;
 
 public interface CartItemRepository extends CrudRepository<CartItem, Integer> {
 
+	@Query(value = "SELECT * FROM CART_ITEM INNER JOIN CART ON CART_ITEM.CART_ID = CART.ID WHERE CART_ITEM.CART_ID = :cartId", nativeQuery = true)
+	List<CartItem> findAllCartItemsByCartId(@Param("cartId") String cartId);
 }
