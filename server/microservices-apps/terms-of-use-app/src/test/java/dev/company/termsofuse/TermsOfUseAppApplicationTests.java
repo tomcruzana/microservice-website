453fab5646d5736620ef8bec9a9ebc78a62f4411
tomcruzana <tomcruzana@ymail.com>
2022-05-12 10:52:15 -0700

    added aop logging to terms-of-use-app

--- server/microservices-apps/terms-of-use-app/src/test/java/dev/company/termsofuse/TermsOfUseAppApplicationTests.java
+++ server/microservices-apps/terms-of-use-app/src/test/java/dev/company/termsofuse/TermsOfUseAppApplicationTests.java
@@ -1,16 +1,21 @@
 package dev.company.termsofuse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import dev.company.termsofuse.dto.TermsOfUseDto;
 import dev.company.termsofuse.entity.TermsOfUse;
 import dev.company.termsofuse.repository.TermsOfUseRepository;
+import dev.company.termsofuse.service.TermsOfUseService;
 
 @SpringBootTest
 class TermsOfUseAppApplicationTests {
+	@Autowired
+	private TermsOfUseService termsOfUseService;
 
 	@Autowired
 	TermsOfUseRepository termsOfUseRepo;
@@ -28,6 +33,14 @@ class TermsOfUseAppApplicationTests {
 		long size = all.spliterator().getExactSizeIfKnown();
 		
 		assertEquals(size, 1);
+	}
+	
+	@Test
+	public void getResourceServiceLayerTest() throws Exception {
+		// fail();
+		int id = 1;
+		TermsOfUseDto termsOfUseDto = termsOfUseService.getTermsOfUse(id);
+		assertNotNull(termsOfUseDto, "Resource is not null!");
 	}
 	
 }
