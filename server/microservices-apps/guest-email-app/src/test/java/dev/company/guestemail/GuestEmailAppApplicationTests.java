b6d5f732d9dc6311dcebb6d98478778f5c934cc7
tomcruzana <tomcruzana@ymail.com>
2022-05-12 10:13:27 -0700

    added aop logging for guest-enail-app

--- server/microservices-apps/guest-email-app/src/test/java/dev/company/guestemail/GuestEmailAppApplicationTests.java
+++ server/microservices-apps/guest-email-app/src/test/java/dev/company/guestemail/GuestEmailAppApplicationTests.java
@@ -7,25 +7,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import dev.company.guestemail.repository.GuestEmailRepository;
+import dev.company.guestemail.service.GuestEmailService;
 
 @SpringBootTest
 class GuestEmailAppApplicationTests {
-	
+
+	@Autowired
+	private GuestEmailService guestEmailService;
+
 	@Autowired
 	private GuestEmailRepository guestEmailRepo;
 
 	@Test
 	void contextLoads() {
 	}
-	
+
 	@Test
 	public void getTotalGuestEmailstest() {
 		// fail();
-		
+
 		// change this value to the actual count of guest emails
 		final int ACTUAL_COUNT = 1;
 		Integer count = guestEmailRepo.findTotal();
 		assertEquals(count, ACTUAL_COUNT);
+	}
+
+	@Test
+	public void getResourceServiceLayerTest() throws Exception {
+		// fail();
+		String allGuestEmails = guestEmailService.getAllGuestEmails();
+		assertEquals(Integer.parseInt(allGuestEmails), guestEmailRepo.count());
 	}
 
 }
