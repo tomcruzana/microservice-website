fad23d9432c68fec7e4aa3dc8dd899eb996427b2
tomcruzana <tomcruzana@ymail.com>
2022-05-12 08:40:20 -0700

    create service layer test for coupon-app

--- server/microservices-apps/coupon-app/src/test/java/dev/company/coupon/CouponAppApplicationTests.java
+++ server/microservices-apps/coupon-app/src/test/java/dev/company/coupon/CouponAppApplicationTests.java
@@ -1,6 +1,7 @@
 package dev.company.coupon;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
@@ -9,11 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import dev.company.coupon.dto.CouponDto;
 import dev.company.coupon.entity.Coupon;
 import dev.company.coupon.repository.CouponRepository;
+import dev.company.coupon.service.CouponService;
 
 @SpringBootTest
 class CouponAppApplicationTests {
+
+	@Autowired
+	private CouponService couponService;
 
 	@Autowired
 	CouponRepository couponRepo;
@@ -29,13 +35,21 @@ class CouponAppApplicationTests {
 		Optional<Coupon> coupon = couponRepo.findByCode(code);
 		assertTrue(!coupon.isEmpty(), "Coupon exists!");
 	}
-	
+
 	@Test
 	public void checkCouponIfActiveTest() {
 		// fail();
 		String code = "chicky90".toUpperCase();
 		Optional<Coupon> coupon = couponRepo.findByCode(code);
 		assertFalse(coupon.get().isActive(), "Coupon is inactive!");
+	}
+
+	@Test
+	public void getResourceServiceLayerTest() throws Exception {
+		// fail();
+		String code = "chicky90".toUpperCase();
+		CouponDto couponDto = couponService.getCoupon(code);
+		assertNotNull(couponDto, "Resource is not null!");
 	}
 
 }
