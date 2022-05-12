cb1926e2085bfb1a5fcbc635e18c5d0d8cc0c29e
tomcruzana <tomcruzana@ymail.com>
2022-05-12 07:28:30 -0700

    created aop advises for service & repo layers

--- server/microservices-apps/about-us-app/src/main/java/dev/company/aboutus/util/ServiceLoggingAdvise.java
+++ server/microservices-apps/about-us-app/src/main/java/dev/company/aboutus/util/ServiceLoggingAdvise.java
@@ -1,18 +1,21 @@
 package dev.company.aboutus.util;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import dev.company.aboutus.service.AboutUsServiceImpl;
 
+@Order(1)
 @Aspect
 @Component
-public class LoggingAdvise<T> {
+public class ServiceLoggingAdvise<T> {
 
 	private final Logger serviceLogger = LoggerFactory.getLogger(AboutUsServiceImpl.class);
 
@@ -56,16 +59,40 @@ public class LoggingAdvise<T> {
 	}
 
 	// SERVICE LAYER ADVISES
+	@AfterThrowing(pointcut = "pointCutTemplateServiceForPost() || pointCutTemplateServiceForCreate()", throwing = "throwingException")
+	public void afterServiceExceptionCallForCreate(Throwable throwingException) {
+		serviceLogger.error("an exception occured when creating the resource -" + throwingException);
+	}
+
+	@AfterThrowing(pointcut = "pointCutTemplateServiceForGet() || pointCutTemplateServiceForRead()", throwing = "throwingException")
+	public void afterServiceExceptionCallForRead(Throwable throwingException) {
+		serviceLogger.error("an exception occured when reading the resource -" + throwingException);
+	}
+
+	@AfterThrowing(pointcut = "pointCutTemplateServiceForPut() || pointCutTemplateServiceForPatch() || pointCutTemplateServiceForUpdate()", throwing = "throwingException")
+	public void afterServiceExceptionCallForUpdate(Throwable throwingException) {
+		serviceLogger.error("an exception occured when updating the resource -" + throwingException);
+	}
+
+	@AfterThrowing(pointcut = "pointCutTemplateServiceForDelete()", throwing = "throwingException")
+	public void afterServiceExceptionCallForDelete(Throwable throwingException) {
+		serviceLogger.error("an exception occured when deleting the resource -" + throwingException);
+	}
+
 	@SuppressWarnings("unchecked")
 	@Around("pointCutTemplateService()")
-	public T beforeServiceMethodCall(ProceedingJoinPoint jp) throws Throwable {
+	public T aroundServiceMethodCall(ProceedingJoinPoint jp) throws Throwable {
+		// get method signature of the current method
 		String methodName = jp.getSignature().getName();
 
-		serviceLogger.info(methodName + " method call start.");
+		// get method args size of the current method
+		int argSize = jp.getArgs().length;
+
+		serviceLogger.info(methodName + " method with " + argSize + " arg(s) call start.");
 
 		T result = (T) jp.proceed();
 
-		serviceLogger.info(methodName + " method call end.");
+		serviceLogger.info(methodName + " method with " + argSize + " arg(s) call end.");
 
 		return (T) result;
 	}
