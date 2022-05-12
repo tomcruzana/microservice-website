40a47c99e504354844d26271ed213a5aeb7c1e57
tomcruzana <tomcruzana@ymail.com>
2022-05-12 09:48:00 -0700

    added service layer test

--- server/microservices-apps/faq-app/src/test/java/dev/company/faq/FaqAppApplicationTests.java
+++ server/microservices-apps/faq-app/src/test/java/dev/company/faq/FaqAppApplicationTests.java
@@ -1,16 +1,25 @@
 package dev.company.faq;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import dev.company.faq.dto.FaqDto;
 import dev.company.faq.entity.Faq;
 import dev.company.faq.repository.FaqRepository;
+import dev.company.faq.service.FaqService;
 
 @SpringBootTest
 class FaqAppApplicationTests {
+
+	@Autowired
+	private FaqService faqService;
 
 	@Autowired
 	FaqRepository faqRepository;
@@ -29,6 +38,13 @@ class FaqAppApplicationTests {
 
 		// perform SQL aggregate count query
 		assertEquals(size, faqRepository.count());
+	}
+
+	@Test
+	public void getResourceServiceLayerTest() throws Exception {
+		// fail();
+		List<FaqDto> allFaqs = faqService.getAllFaqs();
+		assertNotNull(allFaqs, "Resource is not null!");
 	}
 
 }
