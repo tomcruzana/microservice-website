6148672469ea0fa84893a7f62a319ed648a4c379
tomcruzana <tomcruzana@ymail.com>
2022-05-13 08:47:28 -0700

    implemented eureka server

---
+++ server/microservices-apps/eureka-server-app/src/test/java/dev/company/server/EurekaServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.company.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
