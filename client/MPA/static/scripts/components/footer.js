09c0076652a05633f7ab7b9c78bf000eec5993d3
tomcruzana <tomcruzana@ymail.com>
2022-05-16 20:52:23 -0700

    added skeleton loader beta

---
+++ client/MPA/static/scripts/components/footer.js
@@ -0,0 +1,86 @@
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="nav bg-dark text-light mt-4 py-3">
+        <div class="container">
+            <div class="row">
+            <!--footer additional links -->
+            <div class="col my-auto d-flex">
+                <a class="nav-link" href="#">Site Map</a>
+                <a class="nav-link" href="#">Careers</a>
+                <a class="nav-link" href="terms_of_use.html">Terms of Use</a>
+                <a class="nav-link" href="privacy_policy.html">Privacy Policy</a>
+            </div>
+            <!--footer email subs-->
+            <div class="col bg-white text-dark rounded">
+                <div class="pt-2">
+                <div class="email-list-subscription">
+                    <p class="text-right border-top m-0">
+                    Subscribe to get the latest deals and more.
+                    </p>
+                    <p class="text-right">
+                    We have
+                    <span class="total-subscribed-customers font-weight-bold">0</span>
+                    customers on our mailing lists!
+                    </p>
+                    <form class="form-inline justify-content-end">
+                    <div class="form-group mb-2 mr-3">
+                        <input
+                        type="email"
+                        class="form-control"
+                        id="staticEmail2"
+                        placeholder="enter email address"
+                        />
+                    </div>
+                    <button type="submit" class="btn btn-warning mb-2">
+                        Subscribe!
+                    </button>
+                    </form>
+                </div>
+                <!--footer social media links -->
+                <div class="social-media-link-group text-right mt-2">
+                    <i class="fa-brands fa-facebook-square fa-2x mx-1"></i>
+                    <i class="fa-brands fa-youtube-square fa-2x mx-1"></i>
+                    <i class="fa-brands fa-twitter-square fa-2x mx-1"></i>
+                    <i class="fa-brands fa-instagram-square fa-2x ml-1"></i>
+                </div>
+                </div>
+            </div>
+
+            <div class="w-100"></div>
+
+            <div class="col text-left">
+                <!--footer general info -->
+                <div class="pt-2 footer-text">
+                <p class="align-middle my-0">
+                    <i class="fa-solid fa-copyright"></i> Copyright
+                    <span class="current-date-year">DATE</span>. Company Name. All
+                    Rights Reserved.
+                </p>
+                <p class="align-middle my-0">
+                    <i class="fa-solid fa-envelope"></i>
+                    Developer:
+                    <a class="align-middle my-0" href="mailto: abc@example.com"
+                    >Thomas
+                    </a>
+                </p>
+                </div>
+            </div>
+            <div class="col text-right">
+                <!--footer payment types -->
+                <div class="pt-2 payment-types">
+                <i class="fa-brands fa-cc-visa fa-2x mx-1"></i>
+                <i class="fa-brands fa-cc-discover fa-2x mx-1"></i>
+                <i class="fa-brands fa-cc-mastercard fa-2x mx-1"></i>
+                <i class="fa-brands fa-cc-amex fa-2x mx-1"></i>
+                <i class="fa-brands fa-cc-paypal fa-2x ml-1"></i>
+                </div>
+            </div>
+            </div>
+        </div>
+    </footer>
+        `;
+  }
+}
+
+customElements.define("nav-footer", Footer);
---
+++ client/MPA/static/scripts/components/index.js
@@ -0,0 +1,240 @@
+class Index extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="container">
+        <!-- carousel start-->
+        <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-ride="carousel"
+        >
+        <ol class="carousel-indicators">
+            <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+            ></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img
+                class="d-block w-100"
+                src="https://dummyimage.com/800x400/e2e5e7/fff.png"
+                alt="First slide"
+            />
+            </div>
+            <div class="carousel-item">
+            <img
+                class="d-block w-100"
+                src="https://dummyimage.com/800x400/e2e5e7/fff.png"
+                alt="Second slide"
+            />
+            </div>
+            <div class="carousel-item">
+            <img
+                class="d-block w-100"
+                src="https://dummyimage.com/800x400/e2e5e7/fff.png"
+                alt="Third slide"
+            />
+            </div>
+        </div>
+        <a
+            class="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+        >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a
+            class="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+        >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+
+        <!-- slogan start-->
+        <div
+        class="slogan-jumbotron jumbotron p-1 text-center bg-transparent mt-2 mb-0"
+        >
+        <div class="container">
+            <h1 class="display-5 skeleton hide-text">Welcome to <span class="company-name">XYZ company</span></h1>
+            <p class="lead skeleton hide-text">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            </p>
+        </div>
+        </div>
+
+        <!-- promoted products & search start-->
+        <div class="product-preview mt-2">
+        <!-- product search-->
+        <form class="form-inline my-lg-0 justify-content-center">
+            <input
+            class="form-control mr-sm-2 w-25"
+            type="search"
+            placeholder="Search catalog"
+            aria-label="Search"
+            />
+            <button class="btn btn-warning my-sm-0" type="submit">
+            <i class="fa-solid fa-magnifying-glass"></i> Search
+            </button>
+        </form>
+
+        <!-- promoted products-->
+        <h3 class="mb-3 border-bottom">
+            <i class="fa-solid fa-fire"></i> Trending Now!
+        </h3>
+        <div class="card-deck mt-4">
+            <div class="card">
+            <img
+                class="card-img-top"
+                src="https://dummyimage.com/512x512/e2e5e7/fff.png"
+                alt="Card image cap"
+            />
+            <div class="card-body text-center">
+                <p class="card-text text-truncate skeleton hide-text">
+                Some text inside the xyz card. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat...
+                </p>
+                <a href="#" class="card-link">View item</a>
+            </div>
+            </div>
+            <div class="card">
+            <img
+                class="card-img-top"
+                src="https://dummyimage.com/512x512/e2e5e7/fff.png"
+                alt="Card image cap"
+            />
+            <div class="card-body text-center">
+                <p class="card-text text-truncate skeleton hide-text">
+                Some text inside the xyz card. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat...
+                </p>
+                <a href="#" class="card-link">View item</a>
+            </div>
+            </div>
+            <div class="card">
+            <img
+                class="card-img-top"
+                src="https://dummyimage.com/512x512/e2e5e7/fff.png"
+                alt="Card image cap"
+            />
+            <div class="card-body text-center">
+                <p class="card-text text-truncate skeleton hide-text">
+                Some text inside the xyz card. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat...
+                </p>
+                <a href="#" class="card-link">View item</a>
+            </div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end mt-4">
+            <a href="products_search.html" class="more-products-link"
+            >View more products</a
+            >
+        </div>
+        </div>
+
+        <!-- news & updates start-->
+        <div class="news-and-updates mt-4">
+        <h3 class="mb-3 border-bottom">
+            <i class="fa-solid fa-newspaper"></i> News & Updates
+        </h3>
+        <ul class="list-unstyled">
+            <li class="media">
+            <img
+                class="mr-3"
+                src="https://dummyimage.com/128x128/e2e5e7/fff.png"
+                alt="Generic placeholder image"
+            />
+            <div class="media-body">
+                <h5 class="mt-0 mb-1 skeleton hide-text">List-based media object</h5>
+                <p class="skeleton hide-text">
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+                nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+            </div>
+            </li>
+            <li class="media my-4">
+            <img
+                class="mr-3"
+                src="https://dummyimage.com/128x128/e2e5e7/fff.png"
+                alt="Generic placeholder image"
+            />
+            <div class="media-body">
+                <h5 class="mt-0 mb-1 skeleton hide-text">List-based media object</h5>
+                <p class="skeleton hide-text">
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+                nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+            </div>
+            </li>
+            <li class="media">
+            <img
+                class="mr-3"
+                src="https://dummyimage.com/128x128/e2e5e7/fff.png"
+                alt="Generic placeholder image"
+            />
+            <div class="media-body">
+                <h5 class="mt-0 mb-1 skeleton hide-text">List-based media object</h5>
+                <p class="skeleton hide-text">
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+                vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
+                nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </p>
+            </div>
+            </li>
+        </ul>
+
+        <div class="d-flex justify-content-end">
+            <a href="/news_and_updates.html" class="more-products-link"
+            >Read more news & updates</a
+            >
+        </div>
+        </div>
+        </div>
+      `;
+  }
+}
+
+// process page
+customElements.define("main-content", Index);
+
+async function request(url, settings) {
+  const response = await fetch(url, settings);
+  const json = await response.json();
+
+  return json;
+}
+
+// map & populate fetched data
+request(`https://jsonplaceholder.typicode.com/todos/1`, {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+}).then((resource) => {
+  // map data
+  const companyName = document.querySelector(".company-name");
+
+  // populate data
+  companyName.textContent = resource.title;
+});
