27c83d65e9acda065f51f2da27d536cc95ddcbe3
tomcruzana <tomcruzana@ymail.com>
2022-05-18 20:12:53 -0700

    updated frontend pages

--- client/MPA/static/scripts/utils/datetime_service.js
+++
@@ -1,6 +0,0 @@
-let currentDateYear = document.querySelector(".current-date-year");
-
-window.addEventListener("load", (e) => {
-  const currentYear = new Date().getFullYear();
-  currentDateYear.textContent = currentYear;
-});
